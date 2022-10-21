221b41a85bf0a1a48400c24a165dce947be39ff7
Aaron García <aargarcia@aidacanarias.com>
2022-10-21 08:58:08 +0100

    green : return_0_for_empty_string

--- StringCalculatorKata.Test/StringCalculatorShould.cs
+++ StringCalculatorKata.Test/StringCalculatorShould.cs
@@ -1,8 +1,22 @@
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace StringCalculatorKata.Test
 {
     public class StringCalculatorShould {
+        [Test]
+        public void return_0_for_empty_string() {
+            var numbers = string.Empty;
 
+            var  output = StringCalculator.Add(numbers);
+
+            output.Should().Be(0);
+        }
+    }
+
+    public class StringCalculator {
+        public static int Add(string numbers) {
+            return 0;
+        }
     }
 }
